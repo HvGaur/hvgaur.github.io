cf12236051393bcebd175fbb3222cc2e3995f49c
HvGaur <hvgaurr@gmail.com>
2024-08-21 01:27:59 +0530

    widget added

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './Widget.css';
-// import { FaCpu } from 'react-icons/fa'; // Using a CPU icon from react-icons
+import Smiley from '../assets/w98_utopia_smiley.png';
+import AlbumArt from '../assets/cheatcodes.jpg';
 
 const Widget = () => {
+    
+
+
   return (
     <div className="widget">
       <div className="widget__icon">
-        {/* <FaCpu size={24} /> Adjust size as needed */}
+      <img src={Smiley} alt="Smiley" width={40} height={40} className='smiley__icon' />
       </div>
+      <div className='widget-text'>Hello!<br />this is my portfolio(kinda)</div>
+
       <div className="widget__content">
         cpu
         <div className="widget__progress-bar">
@@ -26,6 +32,17 @@ const Widget = () => {
           <div className="widget__progress vol" style={{ width: '77%' }}></div>
         </div>
       </div>
+
+
+      <a href='https://open.spotify.com/album/4TPi1tGDVXCdQOjn4NSRaB' target="_blank" rel="noopener noreferrer">
+      <div className="widget__now-playing">
+        <img src={AlbumArt} alt="Album Art" className="album-art" />
+        <div className="now-playing__info">
+          <div className="track-name" style={{color: 'black'}}>Cheat Codes</div>
+          <div className="artist-name">Black Thought</div>
+        </div>
+      </div>
+        </a>
     </div>
   );
 };
